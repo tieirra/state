825c2ff096d38da1d5f42d305b85c0a354987a91
Tieirra <167941085+Tieirra77@users.noreply.github.com>
2024-08-21 23:34:02 -0500

    added bell icon

--- .vscode/state2/src/Components/NewComp.js
+++ .vscode/state2/src/Components/NewComp.js
@@ -1,20 +1,44 @@
 import React, { Component } from 'react';
+import bell from "./Bell.png";
 
 class NewComp extends Component {
     constructor (props){
         super(props)
 
         this.state = {
-            message: "subscribe to simplilearn"
+            message: "subscribe to simplilearn",
+            sub: "Subcribe",
+            imageURL:bell
         };
         
     }
 
+
+    styles = {
+        fontStyle: "italic",
+        color: "hotpink",
+      };
+Buttonchange =()=> {
+this.setState({
+    message: "Hit the bell icon to never miss an update! " ,
+    sub: "Subscribed"
+});
+}
+
+
+
     render() {
         return (
             <div className = " App"> 
-            <h3> {this.state.message}</h3>
-                
+            <h3 style={this.styles}> {this.state.message}</h3>
+                <button onClick={this.Buttonchange}> 
+                {this.state.sub}
+                </button>
+
+                <p>
+                    <img style={{ width:"30px", height: "30px" }}src={this.state.imageURL} alt="" /> 
+                </p>
+
             </div>
         );
     }
